42f5737e4e754ce7136dba6d1b313c3d91c8355d
GOfC <guardianofchaos6@gmail.com>
2024-07-09 21:26:26 +0300

    Исправить CodeStyle

--- src/test/InMemoryTaskManagerTest.java
+++ src/test/InMemoryTaskManagerTest.java
@@ -3,12 +3,11 @@ package test;
 import main.manager.*;
 
 import org.junit.jupiter.api.BeforeEach;
+
 public class InMemoryTaskManagerTest extends TaskManagerTest<TaskManager> {
     @BeforeEach
     public void addAll() {
         manager = Managers.getDefault();
-
         super.addAll();
     }
-
 }
