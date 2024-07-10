c527e63f633e30d2989c27ace4d56c2c5f28bffb
GOfC <guardianofchaos6@gmail.com>
2024-07-10 12:38:40 +0300

    Исправить тесты

--- src/test/FileBackedTaskManagerTest.java
+++ src/test/FileBackedTaskManagerTest.java
@@ -59,6 +59,12 @@ public class FileBackedTaskManagerTest extends TaskManagerTest<FileBackedTaskMan
     }
 
     @Test
+    public void shouldThrowManagerLoadExceptionIfSetIncorrectFile() {
+        Assertions.assertThrows(ManagerLoadException.class,
+                () -> FileBackedTaskManager.loadFromFile(new File("")));
+    }
+
+    @Test
     public void shouldThrowManagerLoadExceptionIfSetEmptyFile() throws IOException {
         File emptyFile = File.createTempFile("empty", ".txt");
 
